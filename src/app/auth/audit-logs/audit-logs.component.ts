3f3eeb344943be96f2beb3cca2f8274e5c617bec
bellash13 <theobellas@gmail.com>
2024-09-08 08:27:52 -0700

    empty auth components and services

---
+++ src/app/auth/audit-logs/audit-logs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audit-logs',
+  standalone: true,
+  imports: [],
+  templateUrl: './audit-logs.component.html',
+  styleUrl: './audit-logs.component.scss'
+})
+export class AuditLogsComponent {
+
+}
